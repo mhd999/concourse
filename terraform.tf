6a0488a95a1ad008dfb11b97939a3a752068f8f5
Mohamad al moualem <m.almouallem@gmail.com>
2018-11-19 11:38:32 +0100

    Test terraform vars

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "b" {
-  bucket = "${var.TF_VAR_V1}"
+  bucket = "${var.V1}"
   acl    = "private"
 
   tags {
-    Name        = "${var.TF_VAR_V2}"
-    Environment = "${var.TF_VAR_V3}"
+    Name        = "${var.V2}"
+    Environment = "${var.V3}"
   }
 }
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "V1" {}
+variable "V2" {}
+variable "V3" {}
